71aee614504979d5bbc5bf82b13f1bb13f70c5ec
curtcharlton <109427193+curtcharlton@users.noreply.github.com>
2022-07-16 17:18:57 +0200

    Conditionals
    Test to add an existing file to the respository

---
+++ Conditionals.py
@@ -0,0 +1,9 @@
+def perfect_day(temperature):
+    if 20 < temperature < 25: # logical operators used to simplify code
+            print("It is a perfect day")
+
+perfect_day(24)
+            
+    
+    
+    
